437476eb81502abd7d9ee75684f0962012c303c1
onsuk <ondaesuk93@gmail.com>
2019-01-25 00:47:51 +0900

    [Leetcode] Add 976-largest-perimeter-triangle with python || Edit

--- Leetcode/976-largest-perimeter-triangle/largest-perimeter-triangle.py
+++ Leetcode/976-largest-perimeter-triangle/largest-perimeter-triangle.py
@@ -1,11 +1,9 @@
 class Solution:
     def largestPerimeter(self, A):
-        A.sort()
-        A.reverse()
-        for _ in range(len(A)):
-            while((len(A)>2)):
-                if A[0] < (A[1]+A[2]):
-                    return sum(A[:3])
-                else:
-                    del A[0]
+        A.sort(reverse=True)
+        while((len(A)>2)):
+            if A[0] < (A[1]+A[2]):
+                return sum(A[:3])
+            else:
+                A.pop(0)
         return 0
