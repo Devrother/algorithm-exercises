09593a309c5472ba1be558f830c689d1087ee30c
SeungUkLee <lsy931106@gmail.com>
2020-03-10 07:55:22 +0900

    add: programmers 위장 #67

---
+++ Programmers/연습문제/위장/SeungukLee/solution.py
@@ -0,0 +1,17 @@
+from functools import reduce
+from collections import Counter
+from operator import mul
+
+def solution(clothes):
+    clothes_counter = get_clothes_counter(clothes)
+    return get_cnt_combined_clothes(clothes_counter)
+
+
+def get_clothes_counter(clothes):
+    return Counter([kind for name, kind in clothes])
+
+
+def get_cnt_combined_clothes(clothes):
+    clothes_cnt_gen = (cnt for cnt in clothes.values())
+    add_one = lambda x: x + 1
+    return reduce(mul, map(add_one, clothes_cnt_gen)) - 1
