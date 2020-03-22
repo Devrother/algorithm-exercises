31fa973f7c9cc31446ee3ef22c346c980a5c698f
SeungUkLee <lsy931106@gmail.com>
2020-03-22 21:50:05 +0900

    add: programmers 가장 큰 정사각형 찾기 #72

---
+++ Programmers/연습문제/가장 큰 정사각형 찾기/SeungukLee/solution.py
@@ -0,0 +1,30 @@
+def solution(board):
+    row_len, col_len = len(board), len(board[0])
+    
+    if is_all_zero_in_board(board):
+        return 0
+    
+    max_v = 1
+    for i in range(1, row_len):
+        for j in range(1, col_len):
+            if board[i][j] == 0:
+                continue
+            board[i][j] = get_around_min(board, (i, j)) + 1
+            max_v = max(max_v, board[i][j])
+
+    return max_v ** 2
+
+
+def is_all_zero_in_board(board):
+    for row in board:
+        for col in row:
+            if col == 1:
+                return False
+    return True
+
+
+def get_around_min(board, location):
+    i, j = location
+    left, up, left_up = board[i][j-1], board[i-1][j], board[i-1][j-1]
+    return min(left, up, left_up);
+
